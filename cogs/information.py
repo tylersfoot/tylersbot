cac1df2f2790a7223d59ca5ade4469ba5919cfb1
tylersfoot <tylerdepa8@gmail.com>
2022-12-01 00:06:44 -0500

    Converted to slash commands

--- cogs/information.py
+++ cogs/information.py
@@ -17,16 +17,17 @@ class Information(commands.Cog):
     async def account_creation_date(self, ctx, member: discord.Member):  # user commands return the member
         await ctx.respond(f"{member.name}'s account was created on {member.created_at}")
 
-    @commands.command()
+    @commands.slash_command(name="server_count", description="Gets the bot's server count.", guilds=guilds)
     async def servercount(self, ctx):
-        await ctx.send(f'I am in {get_servercount()} servers! (Count refreshes every time bot is restarted)')
+        await ctx.respond(f'I am in {get_servercount()} servers! (Count refreshes every time bot is restarted)')
 
-    @commands.command(aliases=['invlink', 'invite', 'botinvite', 'botinv'])
+    @commands.slash_command(name="invite_link", description="Sends the invite link for the tylersbot server.", guilds=guilds)
     async def invitelink(self, ctx):
-        await ctx.send(f'''Bot invite link: https://discordapp.com/oauth2/authorize?bot_id={self.bot.user.id}&scope=bot&permissions=8
-        Bot server invite link: https://discord.gg/DKpCvsJ4fp''')
+        await ctx.respond(f'''tylersbot invite link: https://discordapp.com/oauth2/authorize?client_id={self.bot.user.id}scope=applications.commands%20bot&permissions=8
+        tylersbot development server invite link: https://discord.gg/DKpCvsJ4fp''')
 
-    @commands.command()
+    @commands.slash_command(name="avatar", description="Sends the avatar of the specified user.", guilds=guilds)
+    # @option("channel", discord.Member, description="Select a user", )
     async def avatar(self, ctx, member: discord.Member = None):
         if member is None:
             member = ctx.author
@@ -36,10 +37,10 @@ class Information(commands.Cog):
             color=int(str(ctx.author.color)[1:], 16)
         )
         avatarEmbed.set_image(url=f"{icon_url}")
-        avatarEmbed.timestamp = ctx.message.created_at
-        await ctx.send(embed=avatarEmbed)
+        avatarEmbed.timestamp = datetime.datetime.now()
+        await ctx.respond(embed=avatarEmbed)
 
-    @commands.command()
+    @commands.slash_command(name="server_info", description="Sends information about the current server.", guilds=guilds)
     async def serverinfo(self, ctx):
         fa = False
         if ctx.guild.mfa_level == 1:
@@ -51,7 +52,7 @@ class Information(commands.Cog):
             color=int(str(ctx.author.color)[1:], 16)
         )
         embed.set_thumbnail(url=ctx.guild.icon.url)
-        embed.timestamp = ctx.message.created_at
+        embed.timestamp = datetime.datetime.now()
         embed.add_field(
             name="Verification",
             value=f'Level: {ctx.guild.verification_level}'
@@ -87,48 +88,7 @@ class Information(commands.Cog):
                   f'\nHighest role: {ctx.guild.roles[-1].mention}',
             inline=True
         )
-        await ctx.send(embed=embed)
-
-
-'''
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.author == self.bot.user:
-            return
-
-        if message.content == 'Hello' or message.content == 'hello':
-            await message.channel.send('Hi!')
-
-        if message.content == 'react' or message.content == 'React':
-            await message.add_reaction('\U0001F9D0')
-
-        if message.content == '.help':
-            await message.channel.send(
-                f'Stuff you can do:\n'
-                f'- Say \'hello\'\n'
-                f'- Say \'react\'\n'
-                f'- Edit a message'
-                f'- React to a message'
-            )
-
-    @commands.Cog.listener()
-    async def on_message_edit(self, before, after):
-        await before.channel.send(
-            f'{before.author} edited a message.\n'
-            f'Before: {before.content}\n'
-            f'After: {after.content}'
-        )
-        
-    @commands.command()
-    async def eastereggs(self, ctx):
-        await ctx.send('say fnf, play weezer')
-
-    @commands.Cog.listener()
-    async def on_reaction_add(self, reaction, user):
-        await reaction.message.channel.send(
-            f'{user} reacted with {reaction.emoji}.'
-        )
-'''
+        await ctx.respond(embed=embed)
 
 
 def setup(bot):
