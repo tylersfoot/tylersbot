4344ff07464cb5b086459740dba5485013d20d69
tylersfoot <tylerdepa8@gmail.com>
2022-12-01 22:06:20 -0500

    converted to slash commands

--- cogs/calculator.py
+++ cogs/calculator.py
@@ -3,40 +3,41 @@ from discord.ext import commands
 from math import *
 import time
 from humanize import number
+from bot import guilds
 
 
 class Calculator(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.slash_command(name="calculate", description="Calculates the given expression.", guilds=guilds)
     async def calc(self, ctx, operation: str):
         try:
             result = eval(operation.replace(' ',''), {'sqrt': sqrt, 'pow': pow})
-            await ctx.reply(result)
+            await ctx.respond(result)
         except Exception as error:
-            await ctx.reply(f'Error occured: `{error}`')
+            await ctx.respond(f'Error occured: `{error}`\nPlease report to `tylersfoot#8888`')
 
-    @commands.command()
+    @commands.slash_command(name="pi", description="Returns the nth digit of pi (up to 1bil)", guilds=guilds)
     async def pi(self, ctx, n):
-        async with ctx.typing():
-            try:
-                n = int(round(float(n)))
-                start_time = time.time()
-                with open('data/pi-billion.txt', 'r') as file:
-                    data = file.read()
-                digit = data[n+1]
-                file.close()
-                print(f'\nIndexing took {round((time.time()-start_time), 2)}s to run')
-                await ctx.reply(f'The {number.ordinal(n)} digit of 𝜋 is {digit}.')
-            except Exception as error:
-                print(error)
-                if str(error) in {'string index out of range', 'cannot fit \'int\' into an index-sized integer'}:
-                    await ctx.reply('Number is too big or small. Enter a number from 1 to 1 billion.')
-                if str(error) in {f'could not convert string to float: \'{n}\''}:
-                    await ctx.reply('Please enter a number.')
-                else:
-                    await ctx.reply(f'Error occured: `{error}`. Please report this to `tylersfoot`.')
+        await ctx.response.defer(ephemeral=True)
+        try:
+            n = int(round(float(n)))
+            start_time = time.time()
+            with open('data/pi-billion.txt', 'r') as file:
+                data = file.read()
+            digit = data[n+1]
+            file.close()
+            print(f'\nIndexing took {round((time.time()-start_time), 2)}s to run')
+            await ctx.followup.send(f'The {number.ordinal(n)} digit of 𝜋 is {digit}.')
+        except Exception as error:
+            print(error)
+            if str(error) in {'string index out of range', 'cannot fit \'int\' into an index-sized integer'}:
+                await ctx.followup.send('Number is too big or small. Enter a number from 1 to 1 billion.')
+            if str(error) in {f'could not convert string to float: \'{n}\''}:
+                await ctx.followup.send('Please enter a number.')
+            else:
+                await ctx.followup.send(f'Error occured: `{error}`. Please report this to `tylersfoot#8888`.')
 
 
 def setup(bot):
