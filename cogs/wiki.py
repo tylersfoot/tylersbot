0bf9ff5bd0f499ac67a4b40b6d45395123517f35
tylersfoot <tylerdepa8@gmail.com>
2022-12-07 22:35:05 -0500

    changed to global command

--- cogs/wiki.py
+++ cogs/wiki.py
@@ -11,7 +11,7 @@ class Wiki(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.slash_command(name="wikisearch", description="Returns a summary of the wikipedia page specified.", guilds=guilds)
+    @commands.slash_command(name="wikisearch", description="Returns a summary of the wikipedia page specified.")
     async def wikisearch(self, ctx, *, request):
 
         try:
